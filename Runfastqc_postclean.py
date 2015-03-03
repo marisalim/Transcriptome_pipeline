2a7d97d2b0e2602abf5e9a57815640a242c101a2
Marisa Lim <marisa_lim@pacbell.net>
2015-03-03 15:08:51 -0500

    added run fastqc for post cleaning samples

---
+++ Runfastqc_postclean.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Code to run fastqc post trimming and post merging
+
+# import modules
+import os, sys, multiprocessing
+
+# set path
+path = '/Volumes/Trochilidae/TestingDat/Cleanreads/CleanreadsOUT'
+
+ID = 'CGML'
+
+def runfastqc_postclean(element):
+	variables = dict(sample = element)
+	
+	commands = """
+	echo "Processing {sample}"
+	fastqc -o /Volumes/Trochilidae/TestingDat/Cleanreads/FastQC_postclean {sample}
+	""".format(**variables)
+	
+	command_list = commands.split('\n')
+	for cmd in command_list:
+		os.system(cmd)
+		
+def thesamps():
+	myfiles = []
+	for root, dirs, files in os.walk(path):
+		for filenames in files:
+			filepath = os.path.join(root, filenames)
+			if ID in filepath:
+				myfiles.append(filepath)
+			else:
+				continue
+	
+	pool = multiprocessing.Pool()
+	pool.map(runfastqc_postclean, myfiles)
+	
+if __name__ == "__main__":
+	thesamps()
+
