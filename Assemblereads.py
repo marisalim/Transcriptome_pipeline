cf3f8a1861228875d62d6e38e09f8c6f3b6c29b7
Marisa Lim <marisa_lim@pacbell.net>
2015-04-01 15:16:16 -0400

    fixed code

--- Assemblereads.py
+++ Assemblereads.py
@@ -6,34 +6,8 @@ path = '/Volumes/Trochilidae/TestingDat/CleanreadsOUT'
 
 ID = 'CGML'
 
-def assemblereads(element):
+def assemblereads():
 
-	variables = dict(
-	sample = element,
-	read1 = element + '_trinity_left.fq',
-	read2 = element + '_trinity_right.fq'
-	) #name your output
-
-	# Trinity commands
-	# seqType = type of reads, fa or fq
-	# JM = Jellyfish Memory, number of GB of system memory to use for k-mer counting by jellyfish
-	# left = (for paired reads) left reads
-	# right = (for paired reads) right reads 
-	# output = name of directory for output (will be created if it doesn't already exist) default: "/usr/uo/7/mlim/trinity_out_dir"
-	# CPU = number of CPUs to use, default is 2
-	# full_cleanup = only retain the Trinity fasta file, rename as ${output_dir}.Trinity.fasta
-	
-	commands = """
-	Trinity --seqType fq --JM 10G --left {read1} --right {read2} --output {sample} --CPU 12 --full_cleanup
-	""".format(**variables)
-
-	command_list = commands.split("\n")
-	for cmd in command_list:
-		os.system(cmd)
-
-	
-# something like this ... but will have to modify!
-def samplist():
 	myfiles = []
 	for root, dirs, files in os.walk(path):
 		for filenames in files:
@@ -44,4 +18,32 @@ def samplist():
 	
 	mysamps = [i.split('_', 1)[0] for i in myfiles]
 	mysamps = set(mysamps)
+	
+	for element in mysamps:
+		variables = dict(
+		sample = element,
+		read1 = element + '_trinity_left.fq',
+		read2 = element + '_trinity_right.fq'
+		) 
+
+		# Trinity commands
+		# seqType = type of reads, fa or fq
+		# JM = Jellyfish Memory, number of GB of system memory to use for k-mer counting by jellyfish
+		# left = (for paired reads) left reads
+		# right = (for paired reads) right reads 
+		# output = name of directory for output (will be created if it doesn't already exist) default: "/usr/uo/7/mlim/trinity_out_dir"
+		# CPU = number of CPUs to use, default is 2
+		# full_cleanup = only retain the Trinity fasta file, rename as ${output_dir}.Trinity.fasta
+	
+		commands = """
+		Trinity --seqType fq --JM 10G --left {read1} --right {read2} --output {sample} --CPU 12 --full_cleanup
+		""".format(**variables)
+
+		command_list = commands.split("\n")
+		for cmd in command_list:
+			os.system(cmd)
+
+	
+
+	
 
