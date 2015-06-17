2e1aebfea0b2dd6799e0ea91fb854ffb23808c35
Marisa Lim <marisa_lim@pacbell.net>
2015-06-17 18:38:48 -0400

    look at the ones in site class 2

--- plotBEB.r
+++ plotBEB.r
@@ -1,4 +1,4 @@
-# for plotting the Bayesian Empirical Bayes Site class proportions
+# for looking at the Bayesian Empirical Bayes Site class proportions
 # (c)2015 Marisa Lim, Stony Brook University
 
 MLwd = "C:/Users/mcwlim/Desktop/StonyBrook/GrahamLab/Dissertation idea materials/THESIS PROJECTS/Hummingbird_Genomics/Transcriptome pipeline files/Candidate gene files/CladeC_results"
@@ -8,3 +8,8 @@ HBB <- read.csv("HBB_cladeC_BEB.csv", header=T)
 head(HBB)
 SCP2 <- read.csv("SCP2_cladeC_BEB.csv", header=T)
 head(SCP2)
+
+# have large proportion in site class 2 (-3)
+HBB[HBB$Class == "-3",]
+SCP2[SCP2$Class == "-3",]
+
