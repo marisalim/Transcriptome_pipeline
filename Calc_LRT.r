a17360ca3d28e7ef95c0c77ad8d4b68b245ee742
Marisa Lim <marisa_lim@pacbell.net>
2015-09-16 23:26:14 -0400

    edited thresholds for signif LRTs

--- Calc_LRT.r
+++ Calc_LRT.r
@@ -15,28 +15,35 @@ head(testmod)
 colnames(nullmod) <- c("Gene", "lnL")
 colnames(testmod) <- c("Gene", "lnL")
 
+dim(nullmod)
+dim(testmod)
+
 # get gene name
 replacebreak <- gsub(pattern="/", replacement=" ", x=nullmod$Gene)
 splitbreak <- strsplit(replacebreak, split=" ")
 genename1 <- sapply(splitbreak, function(x){
   paste(x[[3]])
 })
-genename1
+head(genename1)
 replacebreak2 <- gsub(pattern="_", replacement=" ", x=genename1)
 splitbreak2 <- strsplit(replacebreak2, split=" ")
 genename <- sapply(splitbreak2, function(x){
   paste(x[[1]])
 })
-genename
+head(genename)
 
 LRTdat <- data.frame("Gene"=genename, "Gene_nullmod"=nullmod$Gene, "lnL_nullmod"=nullmod$lnL, 
                      "Gene_testmod"=testmod$Gene, "lnL_testmod"=testmod$lnL)
 LRTdat$LRT <- 2*(LRTdat$lnL_testmod - LRTdat$lnL_nullmod)
 head(LRTdat)
 
-# Which columns have LRT > 1 
-# To do: set the correct threshold for level of significance
-LRTdat[LRTdat$LRT > 1,]
+# Which columns have significant LRT? (df = 1)
+dim(LRTdat[LRTdat$LRT > 3.84,]) # p=0.05
+dim(LRTdat[LRTdat$LRT > 6.64,]) # p=0.01
+dim(LRTdat[LRTdat$LRT > 10.83,]) # p=0.01
+
+
+# add column with gene name and/or GO terms...KEGG? 
 
 
 
