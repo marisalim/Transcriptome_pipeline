b842329824d35d53791913b918c6c10edab77dfb
Marisa Lim <marisa_lim@pacbell.net>
2015-09-27 00:11:51 -0400

    fixed code to make data matrices, added for MF, BP, and CC; added NMDS code and plots

--- Calc_LRT.r
+++ Calc_LRT.r
@@ -11,8 +11,9 @@ setwd(MLwd)
 # load libraries
 library(biomaRt)
 library("GO.db")
-#library(KEGGREST)
-library(goProfiles)
+library(vegan)
+library(dplyr)
+library(tidyr)
 
 # Read in lnL values for each model
 nullmod <- read.table("lnL_null.txt")
@@ -49,10 +50,12 @@ head(LRTdat)
 
 # plot LRT and significance cutoffs
 rownums <- c(1:163)
+jpeg("LRTplot.jpg", height=5, width=5, units="in", res=500)
 plot(rownums, LRTdat$LRT, pch=20, cex=1.5, xlab="Gene ID", ylab="LRT")
 abline(h=3.84, lwd=2, col="tomato")
 abline(h=6.64, lty=2, lwd=2, col="deepskyblue2")
 abline(h=10.83, lty=4, lwd=2, col="seagreen3")
+dev.off()
 
 # Which columns have significant LRT? (df = 1)
 dim(LRTdat[LRTdat$LRT >= 3.84,]) # p=0.05
@@ -72,26 +75,157 @@ ensembl = useMart("ensembl", dataset="tguttata_gene_ensembl")
 filters = listFilters(ensembl)
 attributes = listAttributes(ensembl)
 
-p0.05_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol'), filters='ensembl_peptide_id', values=p0.05$Gene, mart=ensembl)
-head(p0.05_go)
-dim(p0.05_go)
-p0.05_go2 <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol', 'name_1006', 'namespace_1003'), filters='ensembl_peptide_id', values=p0.05$Gene, mart=ensembl)
+alldata_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol', 'name_1006', 'namespace_1003'), filters='ensembl_peptide_id', values=LRTdat$Gene, mart=ensembl)
+head(alldata_go) #i don't think HBB is here because it doesn't have a zebra finch ensembl id
+alldata_BP <- alldata_go[alldata_go$namespace_1003 == "biological_process",] # not all ensembl ids have BP
+alldata_MF <- alldata_go[alldata_go$namespace_1003 == "molecular_function",] # not all ensembl ids have MF
+alldata_CC <- alldata_go[alldata_go$namespace_1003 == "cellular_component",] # not all ensembl ids have MF
 
-p0.05_go2[p0.05_go2$hgnc_symbol == "TPP1", ]
+head(alldata_MF)
+head(alldata_BP)
+head(alldata_CC)
 
-p0.05_BP <- p0.05_go2[p0.05_go2$namespace_1003 == "biological_process",]
-head(p0.05_BP)
-dat_tab <- table(p0.05_BP$hgnc_symbol, p0.05_BP$name_1006) 
-write.table(dat_tab, "p0.05gotermstable.txt")
-barplot(dat_tab)
+alldata_MF$value <- 1
+alldata_BP$value <- 1
+alldata_CC$value <- 1
 
-nonsig_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol', 'name_1006', 'namespace_1003'), filters='ensembl_peptide_id', values=nonsig_genes$Gene, mart=ensembl)
-head(nonsig_go)
-dim(nonsig_go)
-nonsig_BP <- nonsig_go[nonsig_go$namespace_1003 == "biological_process",]
-nonsig_tab <- table(nonsig_BP$hgnc_symbol, nonsig_BP$name_1006)
-barplot(nonsig_tab)
+dim(alldata_MF)
+dim(alldata_BP)
+dim(alldata_CC)
 
+LRTdat$signif_grp_05 <- ifelse(LRTdat$LRT > 3.84, "signif", "nonsig")
+LRTdat$signif_grp_01 <- ifelse(LRTdat$LRT > 6.64, "signif", "nonsig")
+LRTdat$signif_grp_001 <- ifelse(LRTdat$LRT > 10.83, "signif", "nonsig")
+head(LRTdat)
+
+MFmerge <- merge(x=alldata_MF, y=LRTdat, by.x="ensembl_peptide_id", by.y="Gene")
+dim(MFmerge)
+head(MFmerge)
+BPmerge <- merge(x=alldata_BP, y=LRTdat, by.x="ensembl_peptide_id", by.y="Gene")
+dim(BPmerge)
+head(BPmerge)
+CCmerge <- merge(x=alldata_CC, y=LRTdat, by.x="ensembl_peptide_id", by.y="Gene")
+dim(CCmerge)
+head(CCmerge)
+
+MFdat <- spread(data=MFmerge, key=name_1006, value=value)
+MFdat[is.na(MFdat)] <- 0
+BPdat <- spread(data=BPmerge, key=name_1006, value=value)
+BPdat[is.na(BPdat)] <- 0
+CCdat <- spread(data=CCmerge, key=name_1006, value=value)
+CCdat[is.na(CCdat)] <- 0
+head(CCdat)
+
+# Non-metric multidimensional scaling
+MF_NMDS = metaMDS(MFdat[12:ncol(MFdat)], k=2)
+BP_NMDS = metaMDS(BPdat[12:ncol(BPdat)], k=2)
+CC_NMDS = metaMDS(CCdat[12:ncol(CCdat)], k=2)
+# test different distance metrics
+
+stressplot(MF_NMDS)
+stressplot(BP_NMDS)
+stressplot(CC_NMDS)
+
+
+jpeg("all_NMDS.jpg", height=10, width=10, units="in", res=500)
+par(mfrow=c(3,3))
+ordiplot(MF_NMDS,type="n", main="MF, p=0.05")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(MF_NMDS,type="n", main="MF, p=0.01")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_01, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_01, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(MF_NMDS,type="n", main="MF, p=0.001")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_001, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_001, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(BP_NMDS,type="n", main="BP, p=0.05")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(BP_NMDS,type="n", main="BP, p=0.01")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_01, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_01, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(BP_NMDS,type="n", main="BP, p=0.001")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_001, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_001, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(CC_NMDS,type="n", main="CC, p=0.05")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(CC_NMDS,type="n", main="CC, p=0.01")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_01, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_01, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(CC_NMDS,type="n", main="CC, p=0.001")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_001, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_001, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+dev.off()
+
+jpeg("0.05_NMDS.jpg", height=10, width=15, units="in", res=500)
+par(mfrow=c(1,3))
+ordiplot(MF_NMDS,type="n", main="MF, p=0.05")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(BP_NMDS,type="n", main="BP, p=0.05")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+
+ordiplot(CC_NMDS,type="n", main="CC, p=0.05")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+dev.off()
+
+# individual plots for p=0.05
+jpeg("0.05MF_NMDS.jpg", height=10, width=10, units="in", res=500)
+ordiplot(MF_NMDS,type="n", main="MF, p=0.05")
+orditorp(MF_NMDS,display="species",cex=1.5,col="tomato",air=0.01)
+orditorp(MF_NMDS,display="sites",cex=1,air=0.1)
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(MF_NMDS, groups=MFdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+dev.off()
+
+jpeg("0.05BP_NMDS.jpg", height=10, width=10, units="in", res=500)
+ordiplot(BP_NMDS,type="n", main="BP, p=0.05")
+orditorp(BP_NMDS,display="species",cex=1.5,col="tomato",air=0.01)
+orditorp(BP_NMDS,display="sites",cex=1,air=0.1)
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(BP_NMDS, groups=BPdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+dev.off()
+
+jpeg("0.05CC_NMDS.jpg", height=10, width=10, units="in", res=500)
+ordiplot(CC_NMDS,type="n", main="CC, p=0.05")
+orditorp(CC_NMDS,display="species",cex=1.5,col="tomato",air=0.01)
+orditorp(CC_NMDS,display="sites",cex=1,air=0.1)
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="nonsig", col="green")
+ordihull(CC_NMDS, groups=CCdat$signif_grp_05, draw="polygon", label=T, show.groups="signif", col="blue", lty=2)
+dev.off()
+
+
+
+# -------------------- GO terms for each signif level ---------------------------
+# p0.05_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol'), filters='ensembl_peptide_id', values=p0.05$Gene, mart=ensembl)
+# head(p0.05_go)
+# dim(p0.05_go)
+# p0.05_BP <- p0.05_go2[p0.05_go2$namespace_1003 == "biological_process",]
+# head(p0.05_BP)
+# dat_tab <- table(p0.05_BP$ensembl_peptide_id, p0.05_BP$name_1006) 
+# dat_tab[1:5, 1:5] 
+#   #first column is an unidentified gene - that's why the colname is blank
+#   #out of 80, there are only 60 with known biological_process functions
+# write.csv(dat_tab, "dat_tab.csv")
+# 
+# nonsig_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol', 'name_1006', 'namespace_1003'), filters='ensembl_peptide_id', values=nonsig_genes$Gene, mart=ensembl)
+# head(nonsig_go)
+# dim(nonsig_go)
+# nonsig_BP <- nonsig_go[nonsig_go$namespace_1003 == "biological_process",]
+# nonsig_tab <- table(nonsig_BP$ensembl_peptide_id, nonsig_BP$name_1006)
+# nonsig_tab[1:5, 1:5]
 
 # p0.01_go <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol'), filters='ensembl_peptide_id', values=p0.01$Gene, mart=ensembl)
 # head(p0.01_go)
@@ -106,11 +240,6 @@ barplot(nonsig_tab)
 # p0.001_go2 <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol', 'go_id'), filters='ensembl_peptide_id', values=p0.001$Gene, mart=ensembl)
 # p0.001goterms <- data.frame("go_description"=Term(p0.001_go2$go_id))
 # head(p0.001goterms)
-
-# goal: is there enrichment of certain go categories for biological process?
-# how to separate molecular function vs. biological process vs. cellular component go terms??
-
-#### Try goProfiles?
 
 
 
