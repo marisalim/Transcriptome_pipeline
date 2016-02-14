3b7365911967b7493f9d7d4dada627a814667655
Marisa Lim <marisa_lim@pacbell.net>
2016-02-13 21:06:03 -0500

    added codon length check code

--- 6Run_PAML/1.1checkcodonlength.py
+++ 6Run_PAML/1.1checkcodonlength.py
@@ -5,29 +5,29 @@
 
 import os, shutil
 
-#thefiles = [f for f in os.listdir('./alignments_for_codeml/phylipforpaml')]
+thefiles = [f for f in os.listdir('./alignments_for_codeml/phylipforpaml')]
 print "Check codon length:"
 
-# for myfile in thefiles:
-	# if '.phylip' in myfile:
-		# mygene = myfile.split('.')[0]
-		# myphylip = open('./alignments_for_codeml/phylipforpaml/' + myfile, 'r')
-		# firstline = myphylip.readline()
-		# seqlen = firstline.split()[1]
-		# codonlen = float(seqlen)/3
+for myfile in thefiles:
+	if '.phylip' in myfile:
+		mygene = myfile.split('.')[0]
+		myphylip = open('./alignments_for_codeml/phylipforpaml/' + myfile, 'r')
+		firstline = myphylip.readline()
+		seqlen = firstline.split()[1]
+		codonlen = float(seqlen)/3
 		
-# #		print seqlen
-# #		print codonlen
-# #		print float(seqlen) % 3 != 0
-# #		print float(seqlen) % 3
+#		print seqlen
+#		print codonlen
+#		print float(seqlen) % 3 != 0
+#		print float(seqlen) % 3
 	
-		# if float(seqlen) % 3 != 0:
-			# out1 = open('fix_codons.txt', 'a')
-			# out1.write(mygene + '\n')
+		if float(seqlen) % 3 != 0:
+			out1 = open('fix_codons.txt', 'a')
+			out1.write(mygene + '\n')
 	
-		# else:
-			# continue
-# out1.close()
+		else:
+			continue
+out1.close()
 
 reedit = open('fix_codons.txt', 'r')
 #note os.system doesn't have cp function, so using shutil
