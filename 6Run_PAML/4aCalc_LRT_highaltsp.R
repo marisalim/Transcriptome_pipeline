aa6c5de9b763f75e83933fa739434e738e98f480
Marisa Lim <marisa_lim@pacbell.net>
2016-09-05 20:18:39 -0700

    added qvalue method for fdr

--- 6Run_PAML/4aCalc_LRT_highaltsp.R
+++ 6Run_PAML/4aCalc_LRT_highaltsp.R
@@ -17,10 +17,12 @@
 # load libraries
 library(biomaRt)
 library("GO.db")
+library(qvalue)
+library(ggplot2)
+
 #library(vegan)
 #library(dplyr)
 #library(tidyr)
-library(ggplot2)
 #library(ggrepel)
 
 # set working directory
@@ -102,7 +104,8 @@ getLRT('Pgiga', 'nu_Pgiga_lnL_null.txt', 'nu_Pgiga_lnL_pos.txt', 'Pgiga_lnL_null
 mysp <- c('Acast', 'Cviol', 'Ccoru', 'Mphoe', 'Pgiga')
 LRTdat <- c('Acast_LRT.csv', 'Cviol_LRT.csv', 'Ccoru_LRT.csv', 'Mphoe_LRT.csv', 'Pgiga_LRT.csv')
 
-getFDR <- function(mysp, LRTdat){
+# Benjamini and Hochberg 1995 FDR method
+getFDR_BH <- function(mysp, LRTdat){
   
   d <- data.frame('X'=1, 'Gene'='gene', 'Gene_nullmod'='genenullmod', 'lnL_nullmod'=1, 'Gene_testmod'='genetestmod', 'lnL_testmod'=1, 
                   'LRT'=1, 'Species'='species', 'p.value'=1, 'fdr_test'=1, 'FDR_ok'='yes', 'hgnc_symbol'='hgnc')
@@ -113,10 +116,10 @@ getFDR <- function(mysp, LRTdat){
     
     # extract p-values from LRT, use chi-square distribution
     thepvallist <- list()
-    for(i in 1:nrow(theLRTdat)){
-      theLRT <- theLRTdat$LRT[i]
+    for(j in 1:nrow(theLRTdat)){
+      theLRT <- theLRTdat$LRT[j]
       thepval <- pchisq(theLRT, df=1, lower.tail=FALSE)
-      thepvallist[[i]] <- thepval
+      thepvallist[[j]] <- thepval
     }
     LRTdat_p <- data.frame(theLRTdat, "p-value"=unlist(thepvallist))
     #head(LRTdat_p)
@@ -146,8 +149,12 @@ getFDR <- function(mysp, LRTdat){
     LRTdat_uncorrected <- LRTdat_fdr[LRTdat_fdr$p.value < 0.05,]
     LRTdat_uncorrected$FDR_ok <- 'no'
     
+    # what's up with HBB? can add more candidate genes here later
+    HBB <- LRTdat_fdr[LRTdat_fdr$Gene == 'HBB', ]
+    HBB$FDR_ok <- 'na'
+    
     # merge both dfs - I want to see which genes were significant before p-value correction
-    LRTdatfinal <- rbind(LRTdat_uncorrected, LRTdat_fdr_ok)
+    LRTdatfinal <- rbind(LRTdat_uncorrected, LRTdat_fdr_ok, HBB)
     
     # now get gene names for the LRTdat_fdr_ok genes
     # if usual host not working today...25April16, the above host is an archived
@@ -156,17 +163,93 @@ getFDR <- function(mysp, LRTdat){
     
     LRTdat_fdr_ok_withgenenames <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol'), 
                                          filters='ensembl_peptide_id', values=LRTdatfinal$Gene, mart=ensembl)
-    LRTdat_checkomegas <- merge(x=LRTdatfinal, y=LRTdat_fdr_ok_withgenenames, by.x='Gene', by.y='ensembl_peptide_id')
+    LRTdat_checkomegas <- merge(x=LRTdatfinal, y=LRTdat_fdr_ok_withgenenames, by.x='Gene', by.y='ensembl_peptide_id', all.x=T)
     
     d <- rbind(d, LRTdat_checkomegas)
     
   }
   d2 <- d[-1,]
   
-  write.csv(d2, 'gene_checkomegas.csv')
+  write.csv(d2, 'gene_checkomegas_BHfdr.csv')
   
 }
-getFDR(mysp, LRTdat)
+getFDR_BH(mysp, LRTdat)
+
+# Storey and Tibshirani 2003 FDR method
+getFDR_ST <- function(mysp, LRTdat){
+  
+  d <- data.frame('X'=1, 'Gene'='gene', 'Gene_nullmod'='genenullmod', 'lnL_nullmod'=1, 'Gene_testmod'='genetestmod', 'lnL_testmod'=1, 
+                  'LRT'=1, 'Species'='species', 'p.value'=1, 'qvalues'=1, 'FDR_ok'='yes', 'hgnc_symbol'='hgnc')
+  
+  for(i in 1:length(mysp)){
+    
+    theLRTdat <- read.csv(LRTdat[i], header=T)
+    
+    # extract p-values from LRT, use chi-square distribution
+    thepvallist <- list()
+    for(j in 1:nrow(theLRTdat)){
+      theLRT <- theLRTdat$LRT[j]
+      thepval <- pchisq(theLRT, df=1, lower.tail=FALSE)
+      thepvallist[[j]] <- thepval
+    }
+    LRTdat_p <- data.frame(theLRTdat, "p-value"=unlist(thepvallist))
+    #head(LRTdat_p)
+    
+    # FDR test Storey and Tibshirani 2003 using full dataset 
+    fdr_test <- qvalue(p=LRTdat_p$p.value)
+    #summary(fdr_test)
+    #hist(fdr_test)
+    #plot(fdr_test)
+    fdr_df <- data.frame(qvalues = fdr_test$qvalues)
+    
+    # this dataframe has uncorrected (p.value) and q-values (Storey and Tibshirani method, qvalues)
+    # if you forget how to interpret, see this: https://www.r-bloggers.com/example-9-30-addressing-multiple-comparisons/
+    LRTdat_fdr <- cbind(LRTdat_p, fdr_df) 
+    
+    # plot uncorrected and corrected p-values
+    #ggplot(data=LRTdat_fdr) + 
+    #  geom_line(aes(y=sort(p.value), x=X), col='blue', alpha=0.5, size=2) + 
+    #  geom_line(aes(y=sort(qvalues), x=X), col='tomato', alpha=0.5, size=2) +
+    #  geom_hline(aes(yintercept=0.05), col='black', lty=2) +
+    # geom_hline(aes(yintercept=0.01), col='grey', lty=2) +
+    #  theme_bw() + ggtitle('P-values: uncorrected (blue) \nand ST corrected (red)')
+    #ggsave(paste(mysp, '_pvals.jpg', sep=''), height=5, width=5, units='in', dpi=500)
+    
+    # subset data at significance level < 0.05
+    LRTdat_fdr_ok <- LRTdat_fdr[LRTdat_fdr$qvalues < 0.05,]
+    LRTdat_fdr_ok$FDR_ok <- 'yes'
+    
+    # this is without the correction
+    LRTdat_uncorrected <- LRTdat_fdr[LRTdat_fdr$p.value < 0.05,]
+    LRTdat_uncorrected$FDR_ok <- 'no'
+    
+    # what's up with HBB? can add more candidate genes here later
+    HBB <- LRTdat_fdr[LRTdat_fdr$Gene == 'HBB', ]
+    HBB$FDR_ok <- 'na'
+    
+    # merge both dfs - I want to see which genes were significant before p-value correction
+    LRTdatfinal <- rbind(LRTdat_uncorrected, LRTdat_fdr_ok, HBB)
+    
+    # now get gene names for the LRTdat_fdr_ok genes
+    # if usual host not working today...25April16, the above host is an archived
+    #ensembl = useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset="tguttata_gene_ensembl", host="dec2015.archive.ensembl.org")
+    ensembl = useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset="tguttata_gene_ensembl", host='www.ensembl.org')  
+    
+    LRTdat_fdr_ok_withgenenames <- getBM(attributes=c('ensembl_peptide_id', 'hgnc_symbol'), 
+                                         filters='ensembl_peptide_id', values=LRTdatfinal$Gene, mart=ensembl)
+    LRTdat_checkomegas <- merge(x=LRTdatfinal, y=LRTdat_fdr_ok_withgenenames, by.x='Gene', by.y='ensembl_peptide_id', all.x=TRUE)
+    
+    d <- rbind(d, LRTdat_checkomegas)
+    
+  }
+  d2 <- d[-1,]
+  
+  write.csv(d2, 'gene_checkomegas_STfdr.csv')
+  
+}
+getFDR_ST(mysp, LRTdat)
+
+# Note: the BH and ST methods for calculating correct p-value or qvalue yield same results.
 
 # Look at gene_checkomegas.csv to check if codeml outputs for omega make sense
 
