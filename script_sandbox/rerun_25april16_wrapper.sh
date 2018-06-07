8eae81abeeda6efcb864f117d1c9e694ebc1128e
Marisa Lim <marisa_lim@pacbell.net>
2018-06-07 11:01:54 +0200

    moved old paml files to sandbox

---
+++ script_sandbox/rerun_25april16_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+#PBS -l nodes=1:ppn=15 
+#PBS -l walltime=24:00:00 
+#PBS -j oe 
+#PBS -q batch 
+#PBS -m e 
+#PBS -M marisa.lim@stonybrook.edu 
+#PBS -N nudna_reruns
+#
+set echo 
+module load python/2.7.10-mkl 
+module load paml/4.9a
+# run codeml
+python /crucible/bi4iflp/mlim/Run_codeml_nudna/rerun_25april16.py
